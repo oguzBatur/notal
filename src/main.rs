f5e6033f1d2a11f71f6eb01558cf4f772753fa32
oguzBatur <sariozbatur@gmail.com>
2022-04-14 02:04:21 +0300

    Organized functions

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,44 @@ use gl33::*;
 // Local crates.
 mod inputs;
 mod markdown;
+//TODO küçük soyutlamalar ile ilerle.
 
 // Sürekli Değerler - Constant Variables.
 const DEFAULT_WINDOW_SIZE: [u32; 2] = [800, 600];
+const VERT_SHADER: &str = r#"#version 330 core
+  layout (location = 0) in vec3 pos;
+      void main() {
+        gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
+          }
+    "#;
+const FRAG_SHADER: &str = r#"#version 330 core
+  out vec4 final_color;
+    
+      void main() {
+        final_color = vec4(1.0, 0.5, 0.2, 1.0);
 
+          }
+    "#;
 // Types
 #[warn(dead_code)]
 type Vertex = [f32; 3]; // tipik bir vertex. -  a typical vertex.
 type Rgba = (f32, f32, f32, f32); // KMYA renk paleti - RGBA palette type
+
+// ViewPort Struct
+struct ViewPort {
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+}
+
+//RGB renk enum.
 enum Color {
     RGBA(Rgba),
 }
 
 // Main is here.
 fn main() {
-    inputs::take_continous_input();
     // SDL'i aç. - Turn on SDL.
     let sdl = init_sdl();
     // Pencere Yarat. - Create Window.
@@ -60,7 +83,6 @@ fn create_window_default(sdl: &SDL) -> GlWindow {
         .expect("pencere yaratilamadi");
     // Renkler max 1.0 olması lazım, renk uygularken 255' e bölersek rengi alıyoruz ...
     const BG_COLOR: Rgba = (184.0 / 255.0, 213.0 / 255.0, 238.0 / 255.0, 1.0);
-    let mut gl_elements_tuple: (u8, u8) = (0, 0);
 
     unsafe {
         let vertices: [Vertex; 6] = [
@@ -71,11 +93,15 @@ fn create_window_default(sdl: &SDL) -> GlWindow {
             [0.5 / 2.0, 0.5 * f32::sqrt(3.0) / 6.0, 0.0],
             [0.0, -0.5 * f32::sqrt(3.0) / 6.0, 0.0],
         ];
-
+        const VIEWPORT: ViewPort = ViewPort {
+            x: 0,
+            y: 0,
+            width: 800,
+            height: 600,
+        };
         // Arkaplan buffer'ı yarat. - Create a back buffer.
-        create_opengl_buffer(BG_COLOR);
+        create_opengl_buffer(BG_COLOR, VIEWPORT);
         // Vsync'i aç. - init Vsync.
-        gl_elements_tuple = create_triangle(vertices);
         win.set_swap_interval(SwapInterval::Vsync);
         win.swap_window();
     }
@@ -90,7 +116,8 @@ fn create_window_default(sdl: &SDL) -> GlWindow {
             unsafe {
                 glClearColor(BG_COLOR.0, BG_COLOR.1, BG_COLOR.2, BG_COLOR.3);
                 glClear(GL_COLOR_BUFFER_BIT);
-                glUseProgram(gl_elements_tuple.1 as u32);
+                let program = create_shaders(VERT_SHADER, FRAG_SHADER);
+                glUseProgram(program);
                 glBindVertexArray(gl_elements_tuple.0 as u32);
                 glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0 as *const _);
                 win.swap_window();
@@ -101,16 +128,16 @@ fn create_window_default(sdl: &SDL) -> GlWindow {
 }
 
 // Grafik Yarat. -  Create graphics.
-unsafe fn create_opengl_buffer(rgba: Rgba) {
+unsafe fn create_opengl_buffer(rgba: Rgba, viewport: ViewPort) {
     // viewport of openGL
-    glViewport(0, 0, 800, 600);
+    glViewport(viewport.x, viewport.y, viewport.width, viewport.height);
     // Arka buffer (Pencere) - The back buffer
     glClearColor(rgba.0, rgba.1, rgba.2, rgba.3);
     // Ön buffer (Pencere) - Front Buffer
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-unsafe fn create_triangle(vertex: [Vertex; 6]) -> (u8, u8) {
+unsafe fn create_triangle(vertex: [Vertex; 6]) -> u32 {
     // İlk işlem, vertex array object (VAO) yarat. - First, create a vertex array object. (VAO)
     let mut vao = 0;
     glGenVertexArrays(1, &mut vao);
@@ -152,54 +179,38 @@ unsafe fn create_triangle(vertex: [Vertex; 6]) -> (u8, u8) {
     );
     glEnableVertexAttribArray(0);
 
-    // The Vertex Shader.
+    vao
+}
+
+// Create a shader.
+unsafe fn create_shaders(vertex_shader_source: &str, fragment_shader_source: &str) -> u32 {
+    // Vertex Shader..
     let vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     assert_ne!(vertex_shader, 0);
-    const VERT_SHADER: &str = r#"#version 330 core
-  layout (location = 0) in vec3 pos;
-      void main() {
-        gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-          }
-    "#;
-
     glShaderSource(
         vertex_shader,
         1,
-        &(VERT_SHADER.as_bytes().as_ptr().cast()),
-        &(VERT_SHADER.len().try_into().unwrap()),
+        &(vertex_shader_source.as_bytes().as_ptr().cast()),
+        &(vertex_shader_source.len().try_into().unwrap()),
     );
     glCompileShader(vertex_shader);
 
-    // Fragment Shader.
+    // Fragment Shader..
     let fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
     assert_ne!(fragment_shader, 0);
-
-    const FRAG_SHADER: &str = r#"#version 330 core
-  out vec4 final_color;
-    
-      void main() {
-        final_color = vec4(1.0, 0.5, 0.2, 1.0);
-          }
-    "#;
-
     glShaderSource(
         fragment_shader,
         1,
-        &(FRAG_SHADER.as_bytes().as_ptr().cast()),
-        &(FRAG_SHADER.len().try_into().unwrap()),
+        &(fragment_shader_source.as_bytes().as_ptr().cast()),
+        &(fragment_shader_source.len().try_into().unwrap()),
     );
     glCompileShader(fragment_shader);
 
     let shader_program = glCreateProgram();
-
-    //Attach the shaders to the shader_program.
+    assert_ne!(shader_program, 0);
     glAttachShader(shader_program, vertex_shader);
     glAttachShader(shader_program, fragment_shader);
-    glLinkProgram(shader_program);
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
-    (vao as u8, shader_program as u8)
+    shader_program
 }
-
-// Create a shader.
-fn create_shader(vertex_shader: &str, fragment_shader: &str) {}
