532df31c0e65ee8d1a4c271e6a844430a11fce44
oguzBatur <sariozbatur@gmail.com>
2022-04-15 20:28:09 +0300

    Added raw mode

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,11 +1,24 @@
 use std::io::{self, Read};
+use std::os::unix::io::RawFd;
+use termios::*;
+
 // Take contiunous input from user and format it.
 
 // Sürekli olarak girdi al. - Take continous input.
+fn enable_raw_mode() {
+    let fd: RawFd = 0;
+    assert_eq!(fd, 0);
+    let mut termios = Termios::from_fd(fd);
+    match termios {
+        Ok(mut termio) => tcgetattr(fd, &mut termio),
+        Err() => (),
+    }
+}
+
 pub fn take_continous_input() {
+    // Her ASCII karakter 1 byte.
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
-        println!("{}", c);
         if c == 'q' {
             break;
         }
