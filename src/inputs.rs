7bc41f88fe4bf37724ca10a207db7f0755568ef8
oguzBatur <sariozbatur@gmail.com>
2022-04-19 17:57:15 +0300

    overall changes

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,19 +1,7 @@
 use std::io::{self, Read};
-use std::os::unix::io::RawFd;
-use termios::*;
-
 // Take contiunous input from user and format it.
 
 // Sürekli olarak girdi al. - Take continous input.
-fn enable_raw_mode() {
-    let fd: RawFd = 0;
-    assert_eq!(fd, 0);
-    let mut termios = Termios::from_fd(fd);
-    match termios {
-        Ok(mut termio) => tcgetattr(fd, &mut termio),
-        Err() => (),
-    }
-}
 
 pub fn take_continous_input() {
     // Her ASCII karakter 1 byte.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use beryllium::{self, *};
 use gl33::global_loader::*;
 use gl33::*;
 // Local crates.
@@ -5,11 +6,51 @@ mod inputs;
 mod markdown;
 //TODO küçük soyutlamalar ile ilerle.
 //TODO girdi ve çıktı mantığını çöz ve bunda istediğini aldıktan sonra opengl'e geç.
-
+const BG_COLOR: (f32, f32, f32, f32) = (184.0 / 255.0, 213.0 / 255.0, 238.0 / 255.0, 1.0);
 // Main is here.
 fn main() {
     inputs::take_continous_input();
+    // SDL'i başlat
+    let sdl = SDL::init(InitFlags::Video).expect("SDL baslatilamadi");
+    let pencere = create_window(&sdl);
+    unsafe {
+        load_global_gl(|f_name| pencere.get_proc_address(f_name));
+        draw(&pencere);
+    }
+    'main_loop: loop {
+        while let Some(event) = sdl.poll_events().and_then(Result::ok) {
+            match event {
+                Event::Quit(_) => break 'main_loop,
+                _ => (),
+            }
+            unsafe {
+                draw(&pencere);
+            }
+        }
+    }
 } // Main ends here.
+
+// Çiz.
+unsafe fn draw(window: &GlWindow) {
+    glClearColor(BG_COLOR.0, BG_COLOR.1, BG_COLOR.2, BG_COLOR.3);
+    glClear(GL_COLOR_BUFFER_BIT);
+    window.swap_window();
+}
+
+fn create_window(sdl: &SDL) -> GlWindow {
+    sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
+    sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core)
+        .unwrap();
+    sdl.create_gl_window(
+        "Notal",
+        WindowPosition::Centered,
+        800,
+        600,
+        WindowFlags::Shown,
+    )
+    .expect("Pencere Yaratilamadi")
+}
 
 // Create a shader.
 unsafe fn _create_shaders(vertex_shader_source: &str, fragment_shader_source: &str) -> u32 {
