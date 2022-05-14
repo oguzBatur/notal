1e84db542b595f0327cddbaa7aed23e62e232e22
oguzBatur <sariozbatur@gmail.com>
2022-05-14 23:54:29 +0300

    Changed to iced gui library

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,236 @@
-// Local crates.
-mod greeter_ui;
-mod inputs;
-mod markdown;
-use std::sync::Arc;
-
-use greeter_ui::{notal_greeter_ui, FileState, NotalAppState};
-mod windows;
-use druid::{
-    text::RichText, AppDelegate, AppLauncher, Data, Env, Lens, LocalizedString, WindowDesc,
+use iced::{
+    button,
+    window::{self, Mode},
+    Application, Button, Column, Container, Element, Settings, Text,
 };
-fn main() {
-    let main_window = WindowDesc::new(notal_greeter_ui)
-        .title(WINDOW_TITLE)
-        .window_size((800.0, 600.0));
-    //* Başlangıç durumu */
-    let initial_state = NotalAppState {
-        file_state: FileState {
-            current_file_contents: "".to_string(),
-            file_modified: false,
-            file_opened: false,
-            raw: "".to_string(),
-            rendered: RichText::new(Arc::new("")),
-        },
-    };
-
-    AppLauncher::with_window(main_window)
-        .delegate(Delegate)
-        .use_simple_logger()
-        .launch(initial_state)
-        .expect("Failed to launch the app");
+use native_dialog::{FileDialog, MessageDialog, MessageType};
+use std::path::{Path, PathBuf};
+pub fn main() -> iced::Result {
+    NotalApp::run(Settings {
+        id: Some(String::from("notal_app")),
+        window: iced::window::Settings::default(),
+        flags: (),
+        default_font: None,
+        default_text_size: 20,
+        text_multithreading: false,
+        antialiasing: true,
+        exit_on_close_request: true,
+        try_opengles_first: false,
+    })
 }
 
-//* Uygulama Başlığı. */
-const WINDOW_TITLE: LocalizedString<NotalAppState> = LocalizedString::new("Notal");
+//* Create an image . */
+//* Root of our project. implement application in here. */
+/// The root of the NotalApp
+pub enum NotalApp {
+    Loading(MainMenu),
+    Loaded(TextBuffer), //* Bu noktada, input bufferımız açılmış olmalı. */
+}
 
-//* Uygulama içindeki pencere eventlerini delegate denilen struct üzerinde, AppDelegate implementasyonu ile halledicez. */
-struct Delegate;
-impl AppDelegate<NotalAppState> for Delegate {
-    //* Traitleri structlara implemente ederken, Traitlerin kendi içerilerinde barındırdıkları, fonksiyonları structa özel modifiye edebiliyoruz. */
-    fn command(
-        &mut self,
-        ctx: &mut druid::DelegateCtx,
-        target: druid::Target,
-        cmd: &druid::Command,
-        data: &mut NotalAppState,
-        env: &Env,
-    ) -> druid::Handled {
-        //* Eğer, SAVE_FILE_AS komutu gelirse, ve bir hata olursa printle, yoksa ise druid::Handled::Yes enumu ile kontrol et. */
-        if let Some(file_info) = cmd.get(druid::commands::SAVE_FILE_AS) {
-            if let Err(e) = std::fs::write(file_info.path(), &data.file_state.current_file_contents)
-            {
-                println!("Error Writing File: {}", e);
-            }
-            return druid::Handled::Yes;
-        }
-        if let Some(file_info) = cmd.get(druid::commands::OPEN_FILE) {
-            match std::fs::read_to_string(file_info.path()) {
-                Ok(s) => {
-                    println!("File has been opened");
-                    //TODO şu küçük sıkıntıyı çöz
-                    data.file_state.current_file_contents = s.to_owned();
+/// Mesajlar, update fonksiyonlarında kontrol edilir ve buna göre işlem yapılır
+/// Mesajlar, ui elementlerinin içerisinde kullanılabilir. butona basıldığında şu mesajı yolla gibi.
+#[derive(Debug, Clone)]
+pub enum Message {
+    OpenBuffer, //* Burada, uygulamada, yüklü bir dosya varmı yokmu o belli olacak., Loaderror, bize FileDialogState'i vermeli.
+    CloseBuffer,
+    OpenMenu,
+    MainMenuMessage(MainMenuMessage),
+    TextBufferMessage(TextBufferMessage),
+}
+/// Genel mimari;
+/// eğer uygulamada önceden bir dosya açılmışsa, o zaman dosya aç kısmını geç.
+/// Eğer açılmamışsa, ana menüyü göster. Ana menüde ise, dosya açma tuşu, ve yeni dosya açma tuşu ile beraber bir logo bulunmalı.
+/// Menü için yapılması gerekenler. Root'un Update kısmında, Eğer applikasyon loading'te ise, Loaded yap. bunu yaparken iki seçenek var. ya uygulama yüklenir yada yüklenmez.
+/// Yüklenmez ise, hata verir ve menüyü açma güncellemesi yapar.
+/// Root'un view metodunda, gelen mesaj, Error içeriyorsa, bizim FileDialogState'in implementasyonunu yükle.
+///
+/// Update Kısmı, Alıcı Kısım
+/// View Kısmındaki UI elementleri, Vericiler.
+/// Update, state'i değiştirmekten sorumlumu ? evet. State değilde command yollasa o nereeye gider ?
+///
+impl Application for NotalApp {
+    type Flags = ();
+    type Message = Message;
+    type Executor = iced::executor::Default;
+    fn new(flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
+        (NotalApp::Loading(MainMenu::new()), iced::Command::none())
+    }
+    fn title(&self) -> String {
+        String::from("Notal")
+    }
+    fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
+        match self {
+            NotalApp::Loading(menu) => match message {
+                Message::MainMenuMessage(main_msg) => {
+                    menu.update(main_msg);
                 }
-                Err(e) => {
-                    println!("Error opening file {}", e);
+                _ => (),
+            },
+            NotalApp::Loaded(text_buffer) => match message {
+                Message::TextBufferMessage(txt_msg) => {
+                    text_buffer.update(txt_msg);
                 }
-            }
-            return druid::Handled::Yes;
+                _ => (),
+            },
         }
-        druid::Handled::No
+        iced::Command::none()
+    }
+    fn view(&mut self) -> iced::Element<'_, Self::Message> {
+        println!("Opened the main menu.");
+        match self {
+            //* Bir problem çözüldü. Anlaşılan, Mesajlari birbirine chainlememiz gerekiyor, ve view içerisinde kullanılacak custom widget'in view komutunun kullanılması gerekiyor  */
+            NotalApp::Loading(menu) => Column::new()
+                .push(menu.view().map(Message::MainMenuMessage))
+                .align_items(iced::Alignment::Center)
+                .into(),
+            NotalApp::Loaded(text_buffer) => {
+                Container::new(text_buffer.view().map(Message::TextBufferMessage)).into()
+            }
+        }
+    }
+}
+
+///FileDialogState for **Elm - *State* architechture**
+#[derive(Debug, Clone)]
+pub struct MainMenu {
+    file_dialog_opened: bool,
+    open_file_button: button::State,
+    new_file_button: button::State,
+    file_path: PathBuf,
+}
+#[derive(Clone, Debug)]
+pub enum MainMenuMessage {
+    OpenFilePressed,
+    NewFilePressed,
+}
+impl<'a> MainMenu {
+    fn new() -> Self {
+        MainMenu {
+            file_dialog_opened: false,
+            open_file_button: iced::button::State::new(),
+            new_file_button: iced::button::State::new(),
+            file_path: PathBuf::new(),
+        }
+    }
+    fn update(&mut self, msg: MainMenuMessage) {
+        match msg {
+            MainMenuMessage::NewFilePressed => {
+                println!("New File Pressed");
+            }
+            MainMenuMessage::OpenFilePressed => {
+                self.file_dialog_opened = true;
+                match file_dialog_opener(FileDialogCommands::SHOW_OPEN_FILE_DIALOG) {
+                    Ok(path) => {
+                        self.file_path = path.clone();
+                    }
+                    Err(err) => println!("There is an error: {:?}", err),
+                }
+                self.file_dialog_opened = false;
+                println!("{}", self.file_dialog_opened);
+            }
+        }
+    }
+    fn view(&mut self) -> Element<MainMenuMessage> {
+        Column::new()
+            .push(
+                Button::new(&mut self.open_file_button, Text::new("Dosya Aç"))
+                    .on_press(MainMenuMessage::OpenFilePressed),
+            )
+            .push(
+                Button::new(&mut self.new_file_button, Text::new("Yeni Dosya Aç."))
+                    .on_press(MainMenuMessage::NewFilePressed),
+            )
+            .into()
+    }
+}
+
+/// Checks if the file given is in the true format.
+#[derive(Debug, Clone)]
+enum LoadError {
+    FileError,
+    FormatError,
+}
+
+/// Commands for the FileDialog Opener.
+enum FileDialogCommands {
+    SHOW_SAVE_FILE_DIALOG,
+    SHOW_OPEN_FILE_DIALOG,
+}
+
+/// Error Options for @MainMenu the dialog opener errors.
+#[derive(Debug)]
+enum FileDialogError {
+    FileNotFoundError,
+    SaveLocationNotFoundError,
+}
+
+fn file_dialog_opener(command: FileDialogCommands) -> Result<PathBuf, FileDialogError> {
+    match command {
+        FileDialogCommands::SHOW_OPEN_FILE_DIALOG => {
+            let path = FileDialog::new()
+                .set_location("~/Desktop")
+                .add_filter("Text Dosyası", &["txt"])
+                .add_filter("Markdown Dosyaları", &["md"])
+                .show_open_single_file()
+                .unwrap();
+            match path {
+                Some(path) => Ok(path),
+                None => Err(FileDialogError::FileNotFoundError),
+            }
+        }
+        FileDialogCommands::SHOW_SAVE_FILE_DIALOG => {
+            let path = FileDialog::new()
+                .set_location("~/Desktop")
+                .add_filter("Text Dosyası", &["txt"])
+                .add_filter("Markdown Dosyaları", &["md"])
+                .show_save_single_file()
+                .unwrap();
+            match path {
+                Some(path) => {
+                    println!("{:?}", path.to_str().unwrap());
+                    Ok(path)
+                }
+                None => Err(FileDialogError::SaveLocationNotFoundError),
+            }
+        }
+    }
+}
+/// General UI terminology
+/// Dialog => A pop up window that appears after an action.
+/// Field => An area in the WUI or GUI where you need to enter information.
+/// Pane => An independent arera in the WUI or GUI that you can scroll and resize.
+/// Wizard => A dialog that walks a user through the sequence of steps to perform a particular task.
+
+#[derive(Debug, Clone)]
+pub enum TextBufferMessage {
+    NewInputEntry(String),
+    ChangeFileRequested(PathBuf),
+}
+pub struct TextBuffer {
+    input_line: String,
+    file_contents: String,
+    file_path: PathBuf,
+}
+impl<'a> TextBuffer {
+    fn new() -> Self {
+        Self {
+            input_line: "".to_string(),
+            file_contents: "".to_string(),
+            file_path: PathBuf::new(),
+        }
+    }
+
+    fn update(&mut self, msg: TextBufferMessage) {
+        match msg {
+            TextBufferMessage::NewInputEntry(new_input) => {
+                self.input_line = new_input;
+            }
+            TextBufferMessage::ChangeFileRequested(path) => {
+                self.file_path = path;
+            }
+        }
+    }
+    fn view(&mut self) -> Element<TextBufferMessage> {
+        Column::new().push(Text::new("Empty Buffer")).into()
     }
 }
--- src/windows.rs
+++ src/windows.rs
@@ -6,7 +6,7 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-// create and run a window.
+/// create and run a window.
 pub async fn run() {
     env_logger::init();
     let event_loop = EventLoop::new(); // Creata a new EventLoop for the window.
@@ -36,6 +36,7 @@ pub async fn run() {
                     },
                 ..
             } => *control_flow = ControlFlow::Exit,
+            
             WindowEvent::Resized(physical_size) => {
                 state.resize(*physical_size);
             }
@@ -77,7 +78,8 @@ struct State {
     //* New */
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_vertices: u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 impl State {
@@ -168,26 +170,41 @@ impl State {
         });
         const VERTICES: &[Vertex] = &[
             Vertex {
-                position: [0.0, 0.5, 0.0],
-                color: [1.0, 0.0, 0.0],
-            },
+                position: [-0.0868241, 0.49240386, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // A
             Vertex {
-                position: [-0.5, -0.5, 0.0],
-                color: [0.0, 1.0, 0.0],
-            },
+                position: [-0.49513406, 0.06958647, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // B
             Vertex {
-                position: [0.5, -0.5, 0.0],
-                color: [0.0, 0.0, 1.0],
-            },
+                position: [-0.21918549, -0.44939706, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // C
+            Vertex {
+                position: [0.35966998, -0.3473291, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // D
+            Vertex {
+                position: [0.44147372, 0.2347359, 0.0],
+                color: [0.5, 0.0, 0.5],
+            }, // E
         ];
 
-        let num_vertices = VERTICES.len() as u32;
+        const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Vertex Buffer"),
             contents: bytemuck::cast_slice(VERTICES),
             usage: wgpu::BufferUsages::VERTEX,
         });
 
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(INDICES),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+        let num_indices = INDICES.len() as u32;
         Self {
             surface,
             device,
@@ -197,7 +214,8 @@ impl State {
             //* New *//
             render_pipeline,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 
@@ -251,7 +269,8 @@ impl State {
             render_pass.set_pipeline(&self.render_pipeline);
             //* Vertex buffer ayarlama. İlk değer hangi buffer yerinin kullanılacağı, ikincisi ise bufferın ne kadarının kullanıcağı, bu durumda bütün buffer kullanılıyor(..) */
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1);
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
 
         self.queue.submit(std::iter::once(encoder.finish()));
