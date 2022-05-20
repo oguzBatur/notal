559a47eafbbb37445b64581bf78d83fb15bafdf9
oguzBatur <sariozbatur@gmail.com>
2022-05-20 17:27:40 +0300

    Convert project to druid framework

--- src/inputs.rs
+++ src/inputs.rs
@@ -1,2 +1,9 @@
-mod ropey;
-use ropey;
+use ropey::{Rope, RopeBuilder};
+use std::{fs::File, path::PathBuf};
+
+pub fn open_file_with_rope(file: PathBuf) -> Rope {
+    let text = Rope::from_reader(File::open(file).unwrap()).unwrap();
+    text
+}
+
+//* Using rope science for advanced text manipulation. */
--- src/menu.rs
+++ src/menu.rs
@@ -1,1 +1,13 @@
-
+use druid::{widget::Button, Data, FileDialogOptions, FileSpec, Lens, Widget};
+#[derive(Clone, Data, Lens)]
+struct MainMenuState {
+    file_path: String,
+}
+fn build_menu() -> impl Widget<MainMenuState> {
+    // druid::widget::Button
+    let markdown_files = FileSpec::new("Markdown Dosyaları", &["md"]);
+    let file_dialog_options = FileDialogOptions::new()
+        .allowed_types(vec![markdown_files])
+        .title("Dosya Seç...");
+    let open_file = Button::new("Dosya Aç").on_click(|ctx, data| {});
+}
